26ab74f2a2918db9167861b30143a957eb181f2f
searles9 <donovansearles@gmail.com>
2023-11-29 23:50:51 -0500

    updates

--- 02 - Guess Number Game/Project02/App.js
+++ 02 - Guess Number Game/Project02/App.js
@@ -13,6 +13,7 @@ import AppLoading from "expo-app-loading";
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [gameIsOver, setGameIsOver] = useState(true);
+  const [guessRounds, setGuessRounds] = useState(0);
 
   const [fontsLoaded] = useFonts({
     'open-sans':require('./assets/fonts/OpenSans-Regular.ttf'),
@@ -30,8 +31,14 @@ export default function App() {
     setGameIsOver(false)
   }
 
-  function gameOverHandler() {
+  function gameOverHandler(numberOfRounds) {
     setGameIsOver(true)
+    setGuessRounds(numberOfRounds)
+  }
+
+  function startNewGameHandler() {
+    setUserNumber(null);
+    setGuessRounds(0);
   }
   
   let screen = <StartGameScreen onPickNumber={pickedNumberHandler}/>
@@ -41,7 +48,11 @@ export default function App() {
   }
 
   if (gameIsOver && userNumber) {
-    screen = <GameOverScreen />
+    screen = <GameOverScreen 
+      userNumber={userNumber}
+      roundsNumber={guessRounds}
+      onStartNewGame={startNewGameHandler}
+    />
   }
 
   return (
