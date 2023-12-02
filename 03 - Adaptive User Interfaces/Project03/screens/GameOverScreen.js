08af9d1e3031c05a16323f8cc7f04d181b9a94a6
searles9 <donovansearles@gmail.com>
2023-12-02 13:57:45 -0500

    update

--- 03 - Adaptive User Interfaces/Project03/screens/GameOverScreen.js
+++ 03 - Adaptive User Interfaces/Project03/screens/GameOverScreen.js
@@ -1,14 +1,41 @@
-import { View, Image, Text, StyleSheet, Dimensions } from 'react-native';
+import { 
+  View,
+  Image, 
+  Text, 
+  StyleSheet, 
+  useWindowDimensions, 
+  Dimensions,
+  ScrollView 
+} from 'react-native';
 
 import Title from '../components/ui/Title';
 import PrimaryButton from '../components/ui/PrimaryButton';
 import Colors from '../constants/colors';
 
 function GameOverScreen({ roundsNumber, userNumber, onStartNewGame }) {
+  const {width, height} = useWindowDimensions();
+
+  let imageSize = 300;
+
+  if (width < 380) {
+    imageSize = 150;
+  }
+
+  if (height < 400) {
+    imageSize = 80
+  }
+
+  const imageStyle = {
+    width: imageSize,
+    height: imageSize,
+    borderRadius: imageSize / 2,
+  }
+  
   return (
+    <ScrollView style={styles.screen}>
     <View style={styles.rootContainer}>
       <Title>GAME OVER!</Title>
-      <View style={styles.imageContainer}>
+      <View style={[styles.imageContainer, imageStyle]}>
         <Image
           style={styles.image}
           source={require('../assets/images/success.png')}
@@ -21,6 +48,7 @@ function GameOverScreen({ roundsNumber, userNumber, onStartNewGame }) {
       </Text>
       <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
     </View>
+    </ScrollView>
   );
 }
 
@@ -29,6 +57,9 @@ export default GameOverScreen;
 const deviceWidth = Dimensions.get('window').width
 
 const styles = StyleSheet.create({
+  screen: {
+    flex: 1
+  },
   rootContainer: {
     flex: 1,
     padding: 24,
