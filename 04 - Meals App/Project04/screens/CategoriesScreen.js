a150eae38073bfe7d6394b73139a12937dbfbc0c
searles9 <donovansearles@gmail.com>
2023-12-03 14:07:45 -0500

    updates

---
+++ 04 - Meals App/Project04/screens/CategoriesScreen.js
@@ -0,0 +1,25 @@
+import {CATEGORIES} from '../data/dummy-data'
+import {FlatList} from 'react-native';
+import CategoryGridTile from '../components/CatregoryGridTile';
+
+function renderCategoryItem(itemData) {
+    return (
+        <CategoryGridTile 
+            title={itemData.item.title}
+            color={itemData.item.color}
+        />
+    )
+}
+
+function CategoriesScreen() {
+    return (
+        <FlatList 
+        data={CATEGORIES}
+        keyExtractor={(item) => item.id}
+        renderItem={renderCategoryItem}
+        numColumns={2}
+        />
+    )
+}
+
+export default CategoriesScreen;
