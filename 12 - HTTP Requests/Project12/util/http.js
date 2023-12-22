28eafc22902879f3ccd356a6738281d69de005d3
searles9 <donovansearles@gmail.com>
2023-12-21 23:18:22 -0500

    updates

--- 12 - HTTP Requests/Project12/util/http.js
+++ 12 - HTTP Requests/Project12/util/http.js
@@ -1,9 +1,40 @@
 import axios from "axios";
 
-export function storeExpense(expenseData) {
-  console.log("Adding")
-  axios.post(
-    "https://reactnativecourse-0001-default-rtdb.firebaseio.com/expenses.json",
+// no project setup rn
+const BACKEND_URL =
+  "https://XXX.firebaseio.com";
+
+export async function storeExpense(expenseData) {
+  console.log("Adding");
+  const response = await axios.post(
+    BACKEND_URL + "/expenses.json",
     expenseData
   );
+  const id = response.data.name
+  return id;
+}
+
+export async function fetchExpenses() {
+  const response = await axios.get(BACKEND_URL + "/expenses.json");
+  const expenses = [];
+
+  for (const key in response.data) {
+    const expenseObj = {
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description,
+    };
+    expenses.push(expenseObj);
+  }
+
+  return expenses;
+}
+
+export function updateExpense(id, expenseData) {
+  return axios.put(BACKEND_URL + `/expenses/${id}.json`, expenseData)
+}
+
+export function deleteExpense(id) {
+  return axios.delete(BACKEND_URL + `/expenses/${id}.json`);
 }
