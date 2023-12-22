3616c58384a7cad80289eadc99f066527b12e84b
searles9 <donovansearles@gmail.com>
2023-12-22 14:50:26 -0500

    updates

--- 13 - User Auth/Project13/util/auth.js
+++ 13 - User Auth/Project13/util/auth.js
@@ -12,13 +12,14 @@ async function authenticate(mode, email, password) {
     }
   );
 
-  console.log(response.data.email)
+  const token = response.data.idToken
+  return token;
 }
 
-export async function createUser(email, password) {
-  await authenticate("signUp", email, password);
+export function createUser(email, password) {
+  return authenticate("signUp", email, password);
 }
 
-export async function login(email, password) {
-  await authenticate("signInWithPassword", email, password);
+export function login(email, password) {
+  return authenticate("signInWithPassword", email, password);
 }
